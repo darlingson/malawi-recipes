b0ae9637d008e18b3111c8f7acd1018c696b1269
darlingson <dmakuwila@gmail.com>
2024-02-07 11:59:54 +0200

    installed tailwindcss

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/", methods=["GET"])
 def home(name = None):
     return render_template(
-        "hello.html",
+        "home.html",
         name=name,
         date=datetime.now()
     )
