e32711b17b3da53701165de6ecbfb42de91f75e8
darlingson <dmakuwila@gmail.com>
2024-02-14 23:29:20 +0200

    added authentication with placeholder pages from geeks for geeks

--- server.py
+++ server.py
@@ -2,7 +2,60 @@ import re
 import sqlite3
 from datetime import datetime
 from flask import Flask, redirect, render_template, request, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user
+
 app = Flask(__name__)
+
+app.secret_key = 'secret-key'
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+app.config["SECRET_KEY"] = "secret-key"
+db = SQLAlchemy()
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+class Users(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(250), unique=True, nullable=False)
+    password = db.Column(db.String(250), nullable=False)
+ 
+ 
+db.init_app(app) 
+with app.app_context():
+    db.create_all()
+ 
+@login_manager.user_loader
+def loader_user(user_id):
+    return Users.query.get(user_id)
+
+@app.route('/register', methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        user = Users(username=request.form.get("username"),
+                     password=request.form.get("password"))
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for("login"))
+    return render_template("sign_up.html")
+ 
+ 
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        user = Users.query.filter_by(
+            username=request.form.get("username")).first()
+        if user.password == request.form.get("password"):
+            login_user(user)
+            return redirect(url_for("home"))
+    return render_template("login.html")
+ 
+ 
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for("home"))
 
 def get_db_connection():
     conn = sqlite3.connect('database.db')
